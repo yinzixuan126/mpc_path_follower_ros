c6d4ccadf121c866fad4fcb56d0dd5b383819316
zhanglianchuan <zhanglianchuan@baidu.com>
2019-03-08 10:27:34 +0800

    finished plugin part

--- include/mpc_path_follower/mpc_path_follower_ros.h
+++ include/mpc_path_follower/mpc_path_follower_ros.h
@@ -19,115 +19,116 @@
 #include <base_local_planner/odometry_helper_ros.h>
 #include <base_local_planner/goal_functions.h>
 #include <base_local_planner/latched_stop_rotate_controller.h>
+namespace mpc_local_planner {
+    class Mpc_Path_Follower_Ros : public nav_core::BaseLocalPlanner{
+    public:
+        /**
+         * @brief  Constructor for mpc path follower wrapper
+         */
+        Mpc_Path_Follower_Ros();
+        /**
+         * @brief  Destructor for the wrapper
+         */
+        ~Mpc_Path_Follower_Ros();
+        /**
+         * @brief  Constructs the ros wrapper
+         * @param name The name to give this instance of the trajectory planner
+         * @param tf A pointer to a transform listener
+         * @param costmap The cost map to use for assigning costs to trajectories
+         */
+        void initialize(std::string name, tf::TransformListener* tf,
+                        costmap_2d::Costmap2DROS* costmap_ros);
+        /**
+         * @brief  Given the current position, orientation, and velocity of the robot,
+         * compute velocity commands to send to the base
+         * @param cmd_vel Will be filled with the velocity command to be passed to the robot base
+         * @return True if a valid trajectory was found, false otherwise
+         */
+        bool computeVelocityCommands(geometry_msgs::Twist& cmd_vel);
 
-class Mpc_Path_Follower_Ros : public nav_core::BaseLocalPlanner{
-public:
-    /**
-     * @brief  Constructor for mpc path follower wrapper
-     */
-    Mpc_Path_Follower_Ros();
-    /**
-     * @brief  Destructor for the wrapper
-     */
-    ~Mpc_Path_Follower_Ros();
-    /**
-     * @brief  Constructs the ros wrapper
-     * @param name The name to give this instance of the trajectory planner
-     * @param tf A pointer to a transform listener
-     * @param costmap The cost map to use for assigning costs to trajectories
-     */
-    void initialize(std::string name, tf::TransformListener* tf,
-                    costmap_2d::Costmap2DROS* costmap_ros);
-    /**
-     * @brief  Given the current position, orientation, and velocity of the robot,
-     * compute velocity commands to send to the base
-     * @param cmd_vel Will be filled with the velocity command to be passed to the robot base
-     * @return True if a valid trajectory was found, false otherwise
-     */
-    bool computeVelocityCommands(geometry_msgs::Twist& cmd_vel);
+        /**
+         * @brief  Set the plan that the controller is following
+         * @param orig_global_plan The plan to pass to the controller
+         * @return True if the plan was updated successfully, false otherwise
+         */
+        bool setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan);
 
-    /**
-     * @brief  Set the plan that the controller is following
-     * @param orig_global_plan The plan to pass to the controller
-     * @return True if the plan was updated successfully, false otherwise
-     */
-    bool setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan);
+        /**
+         * @brief  Check if the goal pose has been achieved
+         * @return True if achieved, false otherwise
+         */
+        bool isGoalReached();
 
-    /**
-     * @brief  Check if the goal pose has been achieved
-     * @return True if achieved, false otherwise
-     */
-    bool isGoalReached();
+        bool isInitialized() {
+            return initialized_;
+        }
 
-    bool isInitialized() {
-        return initialized_;
-    }
+    private:
 
-private:
+        inline void publishZeroVelocity(){
+          geometry_msgs::Twist cmd_vel;
+          cmd_vel.linear.x = 0.0;
+          cmd_vel.linear.y = 0.0;
+          cmd_vel.angular.z = 0.0;
+          vel_pub_.publish(cmd_vel);
+        }
+        /**
+         * @brief evaluate a polynominal
+         * @param coefficients and input
+         * @return output of the polynominal
+         */
+        int ClosestWaypoint(double x, double y, nav_msgs::Path global_path)
+        {}
+        /**
+         * @brief evaluate a polynominal
+         * @param coefficients and input
+         * @return output of the polynominal
+         */
+        double polyeval(Eigen::VectorXd coeffs, double x);
 
-    inline void publishZeroVelocity(){
-      geometry_msgs::Twist cmd_vel;
-      cmd_vel.linear.x = 0.0;
-      cmd_vel.linear.y = 0.0;
-      cmd_vel.angular.z = 0.0;
-      vel_pub_.publish(cmd_vel);
-    }
-    /**
-     * @brief evaluate a polynominal
-     * @param coefficients and input
-     * @return output of the polynominal
-     */
-    int ClosestWaypoint(double x, double y, nav_msgs::Path global_path)
-    {}
-    /**
-     * @brief evaluate a polynominal
-     * @param coefficients and input
-     * @return output of the polynominal
-     */
-    double polyeval(Eigen::VectorXd coeffs, double x);
+        /**
+         * @brief fit a polynominal
+         * @param vector x, vector y and order
+         * @return output coefficients
+         */
+        Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
+                                int order);
 
-    /**
-     * @brief fit a polynominal
-     * @param vector x, vector y and order
-     * @return output coefficients
-     */
-    Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
-                            int order);
+        bool mpcComputeVelocityCommands(std::vector<geometry_msgs::PoseStamped>& path, geometry_msgs::Twist& cmd_vel );
 
-    void mpcComputeVelocity(std::vector<geometry_msgs::PoseStamped>& path, geometry_msgs::Twist& cmd_vel );
+        void publishLocalPlan(std::vector<geometry_msgs::PoseStamped>& path);
 
-    void publishLocalPlan(std::vector<geometry_msgs::PoseStamped>& path);
+        void publishGlobalPlan(std::vector<geometry_msgs::PoseStamped>& path);
 
-    void publishGlobalPlan(std::vector<geometry_msgs::PoseStamped>& path);
+        void global_path_CB(const nav_msgs::Path& path);
 
-    void global_path_CB(const nav_msgs::Path& path);
+        tf::TransformListener* tf_; ///< @brief Used for transforming point clouds
 
-    tf::TransformListener* tf_; ///< @brief Used for transforming point clouds
+        // for visualisation, publishers of global and local plan
+        ros::Publisher g_plan_pub_, l_plan_pub_, vel_pub_;
+        ros::Subscriber g_plan_sub, odom_sub;
 
-    // for visualisation, publishers of global and local plan
-    ros::Publisher g_plan_pub_, l_plan_pub_, vel_pub_;
-    ros::Subscriber g_plan_sub, odom_sub;
+        costmap_2d::Costmap2DROS* costmap_ros_;
 
-    costmap_2d::Costmap2DROS* costmap_ros_;
+        bool setup_;
 
-    bool setup_;
+        tf::Stamped<tf::Pose> current_pose_;
 
-    tf::Stamped<tf::Pose> current_pose_;
+        bool initialized_;
 
-    bool initialized_;
+        base_local_planner::OdometryHelperRos odom_helper_;
 
-    base_local_planner::OdometryHelperRos odom_helper_;
+        base_local_planner::LatchedStopRotateController latchedStopRotateController_;
 
-    base_local_planner::LatchedStopRotateController latchedStopRotateController_;
+        std::string odom_topic_;
 
-    std::string odom_topic_;
+        std::vector<geometry_msgs::PoseStamped> global_plan_;
 
-    std::vector<geometry_msgs::PoseStamped> global_plan_;
+        base_local_planner::LocalPlannerUtil planner_util_;
 
-    base_local_planner::LocalPlannerUtil planner_util_;
+        Eigen::Vector3f vel;
+        MPC_Path_Follower mpc_solver;
 
-    Eigen::Vector3f vel;
-    MPC_Path_Follower mpc_solver;
-
+    };
 };
 #endif
--- src/mpc_path_follower_ros.cpp
+++ src/mpc_path_follower_ros.cpp
@@ -5,220 +5,240 @@
  * @version:1.0.1
  * @brief:  interface between mpc and ros navigation
  *********************************************************************/
+
 #include <mpc_path_follower/mpc_path_follower_ros.h>
-Mpc_Path_Follower_Ros::Mpc_Path_Follower_Ros():initialized_(false),
-    odom_helper_("odom"), setup_(false){
+#include <pluginlib/class_list_macros.h>
+//register this planner as a BaseLocalPlanner plugin
+PLUGINLIB_EXPORT_CLASS(mpc_local_planner::Mpc_Path_Follower_Ros, nav_core::BaseLocalPlanner)
+namespace mpc_local_planner{
+    Mpc_Path_Follower_Ros::Mpc_Path_Follower_Ros():initialized_(false),
+        odom_helper_("odom"), setup_(false){
 
-}
+    }
 
-void Mpc_Path_Follower_Ros::initialize(std::__cxx11::string name,
-                                       tf::TransformListener *tf,
-                                       costmap_2d::Costmap2DROS *costmap_ros)
-{
-    if (!isInitialized()) {
+    void Mpc_Path_Follower_Ros::initialize(std::__cxx11::string name,
+                                           tf::TransformListener *tf,
+                                           costmap_2d::Costmap2DROS *costmap_ros)
+    {
+        if (!isInitialized()) {
 
-        ros::NodeHandle private_nh("~/" + name);
-        ros::NodeHandle nh;
-        g_plan_sub = nh.subscribe("global_plan", 1, &Mpc_Path_Follower_Ros::global_path_CB, this);
-        //g_plan_sub = nh.subscribe("/control/speed", 1, &Mpc_Path_Follower_Ros::global_path_CB, this);
-        l_plan_pub_ = private_nh.advertise<nav_msgs::Path>("local_plan", 1);
-        tf_ = tf;
-        costmap_ros_ = costmap_ros;
-        costmap_ros_->getRobotPose(current_pose_);
+            ros::NodeHandle private_nh("~/" + name);
+            ros::NodeHandle nh;
+            g_plan_sub = nh.subscribe("global_plan", 1, &Mpc_Path_Follower_Ros::global_path_CB, this);
+            //g_plan_sub = nh.subscribe("/control/speed", 1, &Mpc_Path_Follower_Ros::global_path_CB, this);
+            l_plan_pub_ = private_nh.advertise<nav_msgs::Path>("local_plan", 1);
+            tf_ = tf;
+            costmap_ros_ = costmap_ros;
+            costmap_ros_->getRobotPose(current_pose_);
 
-        // make sure to update the costmap we'll use for this cycle
-        costmap_2d::Costmap2D* costmap = costmap_ros_->getCostmap();
+            // make sure to update the costmap we'll use for this cycle
+            costmap_2d::Costmap2D* costmap = costmap_ros_->getCostmap();
 
-        if( private_nh.getParam( "odom_topic", odom_topic_ ))
-        {
-            odom_helper_.setOdomTopic( odom_topic_ );
+            if( private_nh.getParam( "odom_topic", odom_topic_ ))
+            {
+                odom_helper_.setOdomTopic( odom_topic_ );
+            }
+
+            initialized_ = true;
+        }
+        else{
+            ROS_WARN("This planner has already been initialized, doing nothing.");
+        }
+    }
+
+    bool Mpc_Path_Follower_Ros::computeVelocityCommands(geometry_msgs::Twist &cmd_vel){
+
+        if ( ! costmap_ros_->getRobotPose(current_pose_)) {
+            ROS_ERROR("Could not get robot pose");
+            return false;
+        }
+        std::vector<geometry_msgs::PoseStamped> transformed_plan;
+        if ( ! planner_util_.getLocalPlan(current_pose_, transformed_plan)) {
+            ROS_ERROR("Could not get local plan");
+            return false;
         }
 
-        initialized_ = true;
-    }
-    else{
-        ROS_WARN("This planner has already been initialized, doing nothing.");
-    }
-}
+        //if the global plan passed in is empty... we won't do anything
+        if(transformed_plan.empty()) {
+            ROS_WARN_NAMED("mpc_local_planner", "Received an empty transformed plan.");
+            return false;
+        }
+        ROS_DEBUG_NAMED("mpc_local_planner", "Received a transformed plan with %zu points.", transformed_plan.size());
 
-bool Mpc_Path_Follower_Ros::computeVelocityCommands(geometry_msgs::Twist &cmd_vel){
-
-    if ( ! costmap_ros_->getRobotPose(current_pose_)) {
-        ROS_ERROR("Could not get robot pose");
-        return false;
-    }
-    std::vector<geometry_msgs::PoseStamped> transformed_plan;
-    if ( ! planner_util_.getLocalPlan(current_pose_, transformed_plan)) {
-        ROS_ERROR("Could not get local plan");
-        return false;
-    }
-
-    //if the global plan passed in is empty... we won't do anything
-    if(transformed_plan.empty()) {
-        ROS_WARN_NAMED("mpc_local_planner", "Received an empty transformed plan.");
-        return false;
-    }
-    ROS_DEBUG_NAMED("mpc_local_planner", "Received a transformed plan with %zu points.", transformed_plan.size());
-
-    if (isGoalReached())
-    {
-        //publish an empty plan because we've reached our goal position
-        publishZeroVelocity();
-    }
-    else
-    {
-        //转到这里计算速度，增加了当前机器人的位置
-        //bool isOk = dwaComputeVelocityCommands(current_pose_, cmd_vel);
-        bool isOk = false;
-        if (isOk)
+        if (isGoalReached())
         {
-            publishGlobalPlan(transformed_plan);
+            //publish an empty plan because we've reached our goal position
+            publishZeroVelocity();
         }
         else
         {
-            ROS_WARN_NAMED("mpc_local_planner", "mpc planner failed to produce path.");
-            std::vector<geometry_msgs::PoseStamped> empty_plan;
-            publishGlobalPlan(empty_plan);
+            //转到这里计算速度，增加了当前机器人的位置
+            bool isOk = mpcComputeVelocityCommands(transformed_plan, cmd_vel);
+            if (isOk)
+            {
+                publishGlobalPlan(transformed_plan);
+            }
+            else
+            {
+                ROS_WARN_NAMED("mpc_local_planner", "mpc planner failed to produce path.");
+                std::vector<geometry_msgs::PoseStamped> empty_plan;
+                publishGlobalPlan(empty_plan);
+            }
+            return isOk;
         }
-        return isOk;
     }
-}
 
-void Mpc_Path_Follower_Ros::mpcComputeVelocity(std::vector<geometry_msgs::PoseStamped> &path, geometry_msgs::Twist &cmd_vel){
+    bool Mpc_Path_Follower_Ros::mpcComputeVelocityCommands(std::vector<geometry_msgs::PoseStamped> &path, geometry_msgs::Twist &cmd_vel){
 
-    tf::Stamped<tf::Pose> robot_vel;
-    odom_helper_.getRobotVel(robot_vel);
-    Eigen::Vector3f vel(robot_vel.getOrigin().getX(),
-                        robot_vel.getOrigin().getY(), tf::getYaw(robot_vel.getRotation()));
+        tf::Stamped<tf::Pose> robot_vel;
+        odom_helper_.getRobotVel(robot_vel);
+        Eigen::Vector3f vel(robot_vel.getOrigin().getX(),
+                            robot_vel.getOrigin().getY(), tf::getYaw(robot_vel.getRotation()));
 
 
-    std::vector<double> waypoints_x;
-    std::vector<double> waypoints_y;
-    std::vector<geometry_msgs::PoseStamped>::iterator it = path.begin();
-    std::vector<geometry_msgs::PoseStamped>::iterator it_end = path.begin();
-    if(path.size() >= 40){
-        it_end = it + 40;
-    } else {
-        it_end = path.end();
-    }
-    while(it != it_end){
-        const geometry_msgs::PoseStamped& w = *it;
-        waypoints_x.push_back(w.pose.position.x);
-        waypoints_y.push_back(w.pose.position.y);
-        double* ptrx = &waypoints_x[0];
-        double* ptry = &waypoints_y[0];
-        Eigen::Map<Eigen::VectorXd> waypoints_x_eig(ptrx, 6);
-        Eigen::Map<Eigen::VectorXd> waypoints_y_eig(ptry, 6);
-        // calculate cte and epsi
-        auto coeffs = polyfit(waypoints_x_eig, waypoints_y_eig, 3);
-        // The cross track error is calculated by evaluating at polynomial at x, f(x)
-        // and subtracting y.
-        //double cte = polyeval(coeffs, x) - y;
-        // Due to the sign starting at 0, the orientation error is -f'(x).
-        // derivative of coeffs[0] + coeffs[1] * x -> coeffs[1]
-        //double epsi = psi - atan(coeffs[1]);
-        double cte = polyeval(coeffs, 0);  // px = 0, py = 0
-        double epsi = -atan(coeffs[1]);  // p
-        Eigen::VectorXd state(6);
-        state << 0, 0, 0, vel[0], cte, epsi;
-        std::vector<double> vars;
-        mpc_solver.initialize();
-        vars = mpc_solver.solve(state, coeffs);
-        std::vector<double> mpc_x_vals;
-        std::vector<double> mpc_y_vals;
-        for (int i = 2; i < vars.size(); i ++) {
-          if (i%2 == 0) {
-            mpc_x_vals.push_back(vars[i]);
-          }
-          else {
-            mpc_y_vals.push_back(vars[i]);
-          }
+        std::vector<double> waypoints_x;
+        std::vector<double> waypoints_y;
+        std::vector<geometry_msgs::PoseStamped>::iterator it = path.begin();
+        std::vector<geometry_msgs::PoseStamped>::iterator it_end = path.begin();
+        //自定义
+        if(path.size() >= 40){
+            it_end = it + 40;
+        } else {
+            it_end = path.end();
         }
-//        steer_value = vars[0];
-//        throttle_value = vars[1];
-
+        while(it != it_end){
+            const geometry_msgs::PoseStamped& w = *it;
+            waypoints_x.push_back(w.pose.position.x);
+            waypoints_y.push_back(w.pose.position.y);
+            double* ptrx = &waypoints_x[0];
+            double* ptry = &waypoints_y[0];
+            Eigen::Map<Eigen::VectorXd> waypoints_x_eig(ptrx, 6);
+            Eigen::Map<Eigen::VectorXd> waypoints_y_eig(ptry, 6);
+            // calculate cte and epsi
+            auto coeffs = polyfit(waypoints_x_eig, waypoints_y_eig, 3);
+            // The cross track error is calculated by evaluating at polynomial at x, f(x)
+            // and subtracting y.
+            //double cte = polyeval(coeffs, x) - y;
+            // Due to the sign starting at 0, the orientation error is -f'(x).
+            // derivative of coeffs[0] + coeffs[1] * x -> coeffs[1]
+            //double epsi = psi - atan(coeffs[1]);
+            double cte = polyeval(coeffs, 0);  // px = 0, py = 0
+            double epsi = -atan(coeffs[1]);  // p
+            Eigen::VectorXd state(6);
+            state << 0, 0, 0, vel[0], cte, epsi;
+            std::vector<double> vars;
+            mpc_solver.initialize();
+            vars = mpc_solver.solve(state, coeffs);
+            if (vars.size() < 2){
+                return false;
+            }
+            std::vector<double> mpc_x_vals;
+            std::vector<double> mpc_y_vals;
+            for (int i = 2; i < vars.size(); i ++) {
+              if (i%2 == 0) {
+                mpc_x_vals.push_back(vars[i]);
+              }
+              else {
+                mpc_y_vals.push_back(vars[i]);
+              }
+            }
+            double steer_value = 0.0, throttle_value = 0.0;
+            steer_value = vars[0];
+            throttle_value = vars[1];
+            //temporarily, cannot tranfer to cmd_vel using acceleration and steering angle
+            //so assume constant velocity 0.3m/s to tranfer to cmd_vel;
+            double radius = 0.0;
+            if (fabs(tan(steer_value)) <= 1e2){
+                radius = 1e5;
+            } else {
+                radius = 3.0 / tan(steer_value);}
+            cmd_vel.angular.z = 0.3 / radius;
+            cmd_vel.linear.x = 0.3;
+            return true;
+        }
     }
-}
 
-bool Mpc_Path_Follower_Ros::setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan){
-    if(!initialized_){
-        ROS_ERROR("Planner utils have not been initialized, please call initialize() first");
-        return false;
-    }
+    bool Mpc_Path_Follower_Ros::setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan){
+        if(!initialized_){
+            ROS_ERROR("Planner utils have not been initialized, please call initialize() first");
+            return false;
+        }
 
-    //reset the global plan
-    global_plan_.clear();
-    ROS_INFO("Got new plan");
-    global_plan_ = orig_global_plan;
+        //reset the global plan
+        global_plan_.clear();
+        ROS_INFO("Got new plan");
+        global_plan_ = orig_global_plan;
 
-    return true;
-}
-
-void Mpc_Path_Follower_Ros::global_path_CB(const nav_msgs::Path& path){
-    if(path.poses.empty())
-        return;
-    std::vector<geometry_msgs::PoseStamped> original_plan;
-    // Extract the plan in world co-ordinates, we assume the path is all in the same frame
-    for(unsigned int i=0; i < path.poses.size(); i++){
-        original_plan[i] = path.poses[i];
-    }
-    setPlan(original_plan);
-}
-
-bool Mpc_Path_Follower_Ros::isGoalReached(){
-    if (! isInitialized()) {
-        ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
-        return false;
-    }
-    if ( ! costmap_ros_->getRobotPose(current_pose_)) {
-        ROS_ERROR("Could not get robot pose");
-        return false;
-    }
-
-    if(latchedStopRotateController_.isGoalReached(&planner_util_, odom_helper_, current_pose_)) {
-        ROS_INFO("Goal reached");
         return true;
-    } else {
-        return false;
-    }
-}
-
-double Mpc_Path_Follower_Ros::polyeval(Eigen::VectorXd coeffs, double x){
-    double result = 0.0;
-    for (int i = 0; i < coeffs.size(); i++) {
-        result += coeffs[i] * pow(x, i);
-    }
-    return result;
-}
-
-Eigen::VectorXd Mpc_Path_Follower_Ros::polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
-                                               int order){
-    assert(xvals.size() == yvals.size());
-    assert(order >= 1 && order <= xvals.size() - 1);
-    Eigen::MatrixXd A(xvals.size(), order + 1);
-
-    for (int i = 0; i < xvals.size(); i++) {
-        A(i, 0) = 1.0;
     }
 
-    for (int j = 0; j < xvals.size(); j++) {
-        for (int i = 0; i < order; i++) {
-            A(j, i + 1) = A(j, i) * xvals(j);
+    void Mpc_Path_Follower_Ros::global_path_CB(const nav_msgs::Path& path){
+        if(path.poses.empty())
+            return;
+        std::vector<geometry_msgs::PoseStamped> original_plan;
+        // Extract the plan in world co-ordinates, we assume the path is all in the same frame
+        for(unsigned int i=0; i < path.poses.size(); i++){
+            original_plan[i] = path.poses[i];
+        }
+        setPlan(original_plan);
+    }
+
+    bool Mpc_Path_Follower_Ros::isGoalReached(){
+        if (! isInitialized()) {
+            ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
+            return false;
+        }
+        if ( ! costmap_ros_->getRobotPose(current_pose_)) {
+            ROS_ERROR("Could not get robot pose");
+            return false;
+        }
+
+        if(latchedStopRotateController_.isGoalReached(&planner_util_, odom_helper_, current_pose_)) {
+            ROS_INFO("Goal reached");
+            return true;
+        } else {
+            return false;
         }
     }
-    auto Q = A.householderQr();
-    auto result = Q.solve(yvals);
-    return result;
-}
 
-void Mpc_Path_Follower_Ros::publishLocalPlan(std::vector<geometry_msgs::PoseStamped>& path) {
-    base_local_planner::publishPlan(path, l_plan_pub_);
-}
+    double Mpc_Path_Follower_Ros::polyeval(Eigen::VectorXd coeffs, double x){
+        double result = 0.0;
+        for (int i = 0; i < coeffs.size(); i++) {
+            result += coeffs[i] * pow(x, i);
+        }
+        return result;
+    }
+
+    Eigen::VectorXd Mpc_Path_Follower_Ros::polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
+                                                   int order){
+        assert(xvals.size() == yvals.size());
+        assert(order >= 1 && order <= xvals.size() - 1);
+        Eigen::MatrixXd A(xvals.size(), order + 1);
+
+        for (int i = 0; i < xvals.size(); i++) {
+            A(i, 0) = 1.0;
+        }
+
+        for (int j = 0; j < xvals.size(); j++) {
+            for (int i = 0; i < order; i++) {
+                A(j, i + 1) = A(j, i) * xvals(j);
+            }
+        }
+        auto Q = A.householderQr();
+        auto result = Q.solve(yvals);
+        return result;
+    }
+
+    void Mpc_Path_Follower_Ros::publishLocalPlan(std::vector<geometry_msgs::PoseStamped>& path) {
+        base_local_planner::publishPlan(path, l_plan_pub_);
+    }
 
 
-void Mpc_Path_Follower_Ros::publishGlobalPlan(std::vector<geometry_msgs::PoseStamped>& path) {
-    base_local_planner::publishPlan(path, g_plan_pub_);
-}
+    void Mpc_Path_Follower_Ros::publishGlobalPlan(std::vector<geometry_msgs::PoseStamped>& path) {
+        base_local_planner::publishPlan(path, g_plan_pub_);
+    }
+};
+
 //int main(int argc, char** argv)
 //{
 //  ros::init(argc, argv, "mpc_path_follower_node");
